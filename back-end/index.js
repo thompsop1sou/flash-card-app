05185fd2217044c87481d2097a58868eee7f7673
thompsop1sou <thompsop1@sou.edu>
2024-03-22 23:08:02 -0700

    Finish up back end server and front end connection

--- back-end/index.js
+++ back-end/index.js
@@ -7,18 +7,29 @@ const server_key = process.env["FLASHCARD_SERVER_KEY"];
 
 app.use(bodyParser.json());
 
-// Open a named flashcard set
+// Open a flashcard set
 app.post("/open", (req, res) => {
-  console.log(req.body);
   console.log(`Trying to open "${req.body.name}"`);
-  res.status(200).send("Success!");
+  let foundIndex = flashcard_sets.findIndex((value, index, array) => value.name === req.body.name);
+  if (foundIndex === -1) {
+    res.status(404).send(JSON.stringify(req.body));
+    console.log(JSON.stringify(req.body));
+  } else {
+    res.status(200).send(JSON.stringify(flashcard_sets[foundIndex]));
+  }
 });
 
-// Save a named flashcard set (may replace an existing one with the same name)
+// Save a flashcard set (may replace an existing one with the same name)
 app.post("/save", (req, res) => {
   console.log(req.body);
   console.log(`Trying to save "${req.body.name}"`);
-  res.status(200).send("Success!");
+  let foundIndex = flashcard_sets.findIndex((value, index, array) => value.name === req.body.name);
+  if (foundIndex === -1) {
+    flashcard_sets.push(req.body);
+  } else {
+    flashcard_sets[foundIndex] = req.body;
+  }
+  res.status(200).send(JSON.stringify(req.body));
 });
 
 app.listen(port, () => {
